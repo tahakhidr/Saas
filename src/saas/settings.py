b2843563c05fe692f9633120f0194de8b0202878
Taha Khidr <tahakhidr@gmail.com>
2024-09-04 15:50:22 +0300

    CSRF_TRUSTED_ORIGINS

--- src/saas/settings.py
+++ src/saas/settings.py
@@ -16,6 +16,7 @@ DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
+CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "https://").split(",")
 
 # Application definition
 INSTALLED_APPS = [
